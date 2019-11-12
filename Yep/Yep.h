ec41ab0b74327061fc1fa5a0333cebdf84d7acef
Jaime Jazareno III <jaime@churinc.com>
2019-11-12 14:59:21 +0800

    Initial Commit

---
+++ Yep/Yep.h
@@ -0,0 +1,19 @@
+//
+//  Yep.h
+//  Yep
+//
+//  Created by Jaime Jazareno III on 11/12/19.
+//  Copyright © 2019 Chur Networks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Yep.
+FOUNDATION_EXPORT double YepVersionNumber;
+
+//! Project version string for Yep.
+FOUNDATION_EXPORT const unsigned char YepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yep/PublicHeader.h>
+
+
