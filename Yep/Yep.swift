2c7c4f55ea4d8fe357ef6a080f0f290449de7f62
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2019-11-12 15:33:51 +0800

    Added test cases

---
+++ Yep/Yep.swift
@@ -0,0 +1,23 @@
+//
+//  Yep.swift
+//  Yep
+//
+//  Created by Jaime Jazareno III on 11/12/19.
+//  Copyright © 2019 Chur Networks. All rights reserved.
+//
+
+import Foundation
+
+public final class Yep {
+
+    let name = "Yep"
+
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+
+}
--- YepTests/YepTests.swift
+++ YepTests/YepTests.swift
@@ -9,26 +9,20 @@
 import XCTest
 @testable import Yep
 
-class YepTests: XCTestCase {
+class SwiftyLibTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  var yep: Yep!
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func setUp() {
+    yep = Yep()
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  func testAdd() {
+    XCTAssertEqual(yep.add(a: 1, b: 1), 2)
+  }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  func testSub() {
+    XCTAssertEqual(yep.sub(a: 2, b: 1), 1)
+  }
 
 }
